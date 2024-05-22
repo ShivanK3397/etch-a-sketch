2d511bd092e3830ba6e76e1b0975a1b71489bc2c
Shivan Kathir <shivan.kathir@gmail.com>
2024-05-22 17:17:55 -0400

    added the color changing effect on hover and fixed bug where button to input number of squares only worked once

--- script.js
+++ script.js
@@ -1,17 +1,50 @@
-function createGrid(){
-    const container = document.querySelector(".grid-container");
-    for (let i =0;i<16;i++){
+const container = document.querySelector(".grid-container");
+let numOfGrids = 0;
+function createGrid(sketchPad,numOfSquares=16){
+    for (let i =0;i<numOfSquares;i++){
         const lineOfGrids = document.createElement("div");
         lineOfGrids.setAttribute("class","line-of-grids")
-        for (let j=0;j<16;j++){
+        for (let j=0;j<numOfSquares;j++){
             const grid = document.createElement("div");
             grid.setAttribute("class","grid")
             lineOfGrids.appendChild(grid);
+            grid.addEventListener("mouseenter",function(){
+                grid.style.backgroundColor="blue";
+            })
 
         }
-        container.appendChild(lineOfGrids);
+        sketchPad.appendChild(lineOfGrids);
 
+
+    }
+    numOfGrids=numOfSquares;
+
+}
+
+function deleteGrid(num){
+    for (i = 0;i<num;i++){
+        container.removeChild(document.querySelector(".line-of-grids"));
     }
 }
 
-createGrid();
+createGrid(container);
+
+const btn = document.querySelector(".input-button");
+const input = document.querySelector("input");
+const body = document.querySelector("body");
+const div = document.querySelector(".input");
+
+
+btn.addEventListener("click",function(){
+    const inputValue=+input.value;
+    input.value="";
+    if (inputValue>100||inputValue===0){
+
+    }
+    else{
+        deleteGrid(numOfGrids);
+        createGrid(container,inputValue);
+        
+
+    }
+}); 
