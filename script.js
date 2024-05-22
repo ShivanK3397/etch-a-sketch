38b86a387933d0adb9d9a19942a6b705d92a54ac
Shivan Kathir <shivan.kathir@gmail.com>
2024-05-22 18:14:33 -0400

    added buttons for setting color of squares as well as button to reset grid

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const container = document.querySelector(".grid-container");
 let numOfGrids = 0;
+let color = "blue"
 function createGrid(sketchPad,numOfSquares=16){
     for (let i =0;i<numOfSquares;i++){
         const lineOfGrids = document.createElement("div");
@@ -9,7 +10,7 @@ function createGrid(sketchPad,numOfSquares=16){
             grid.setAttribute("class","grid")
             lineOfGrids.appendChild(grid);
             grid.addEventListener("mouseenter",function(){
-                grid.style.backgroundColor="blue";
+                grid.style.backgroundColor=color;
             })
 
         }
@@ -48,3 +49,24 @@ btn.addEventListener("click",function(){
 
     }
 }); 
+
+const buttons = document.querySelector(".buttons")
+buttons.addEventListener("click",function(event){
+    const input = event.target.id;
+    console.log(input);
+    if(input==="reset"){
+        deleteGrid(numOfGrids);
+        createGrid(container,numOfGrids);
+    }
+    else if(input==="random"){
+        color=`rgb(${randomRGBValue()},${randomRGBValue()},${randomRGBValue()})`;
+    }
+    else{
+        color=input;
+
+    }
+})
+
+function randomRGBValue(){
+    return Math.floor(Math.random()*254)+1
+}
